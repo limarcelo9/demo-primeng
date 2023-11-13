65bce05f614730b9edad72109fbcf106b6f80c7e
Marcelo lima <limarcelo90@gmail.com>
2023-11-12 23:37:39 -0300

    testes com primeng e progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
 import { Component } from '@angular/core';
+import { ConfirmationService, MessageService, ConfirmEventType, Message } from 'primeng/api';
+import { catchError, finalize  } from 'rxjs/operators';
+import { ReportService } from './report.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ConfirmationService, MessageService]
 })
 export class AppComponent {
   title = 'demo-primeng';
+  totalRegister = 999
+  value: number = 0;
+  constructor(
+    private reportService: ReportService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
+  ) {}
+
+  envioFinal() {
+    console.log('enviou final')
+  }
+
+  validar() {
+    console.log('validou')
+  }
+
+  abrirModal() {
+    this.confirmationService.confirm({
+        message: 'Are you sure that you want to proceed?',
+        header: 'Confirmation',
+        icon: 'pi pi-exclamation-triangle',
+        accept: () => {
+            this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
+        },
+        reject: (type: ConfirmEventType) => {
+            switch (type) {
+                case ConfirmEventType.REJECT:
+                    this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+                    break;
+                case ConfirmEventType.CANCEL:
+                    this.messageService.add({ severity: 'warn', summary: 'Cancelled', detail: 'You have cancelled' });
+                    break;
+            }
+        }
+    });
+  }
+
+  startProgress() {
+    let interval = setInterval(() => {
+      this.consultarPercentualRestante();
+      if (this.value >= 100) {
+          this.value = 100;
+          this.messageService.add({ severity: 'info', summary: 'Success', detail: 'Process Completed' });
+          clearInterval(interval);
+      }
+  }, 3000);
+  }
+  consultarPercentualRestante() {
+    this.gerarRelatorio();
+  }
+
+  gerarRelatorio() {
+    this.reportService.getReports().pipe(
+      catchError((error) => {
+        console.error(error);
+        throw error;
+      }),
+      finalize(() => {
+        console.log('Concluído');
+      })
+    ).subscribe((data: any) => {
+      this.value = data;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { HttpClientModule } from '@angular/common/http';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { MessagesModule } from 'primeng/messages';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { ToastModule } from 'primeng/toast';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    HttpClientModule,
+    ConfirmDialogModule,
+    ButtonModule,
+    MessagesModule,
+    ProgressBarModule,
+    ToastModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
